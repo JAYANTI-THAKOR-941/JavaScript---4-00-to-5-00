3b11639d227dd71f05e9f37bcc3d26c9a7e6bb66
Jayanti Thakor <techeducator8@gmail.com>
2026-05-26 16:36:52 +0530

    product management - if else

---
+++ 12_typeScript.js
@@ -0,0 +1,5 @@
+"use strict";
+let studentName = "Mayur Rabari";
+let age = 20;
+console.log(`studentName : ${studentName}`);
+console.log(`Age : ${age}`);
---
+++ 12_typeScript.ts
@@ -0,0 +1,7 @@
+
+let studentName : string = "Mayur Rabari";
+
+let age : number = 20;
+
+console.log(`studentName : ${studentName}`);
+console.log(`Age : ${age}`);

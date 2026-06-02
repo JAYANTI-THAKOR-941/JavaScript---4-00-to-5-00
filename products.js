aefa8adfd535e0c8d899f4afc7d12f8559364865
JAYANTI-THAKOR-941 <jyantithakor941@gmail.com>
2026-06-02 16:37:25 +0530

    forLoop - Dynamic Product

---
+++ products.js
@@ -0,0 +1,59 @@
+const products = [
+  {
+    name: "Wireless Mouse",
+    desc: "Ergonomic wireless mouse with smooth tracking.",
+    price: 799,
+    category: "Electronics",
+    image: "https://encrypted-tbn2.gstatic.com/shopping?q=tbn:ANd9GcTeDOKovc88AP1nOv2dI0ZW8ofbbn35k9a5VBl5bMkSErpAQahvlh5r_huYLG_lqX8aOcttm56LSTjTKyigV-L94S9nIysT-HKsntpnNFJEhHKUeVNmGxMRjTY"
+  },
+  {
+    name: "Gaming Keyboard",
+    desc: "RGB mechanical keyboard for gamers.",
+    price: 2499,
+    category: "Electronics",
+    image: "https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcQkyJn7c5hXo_xrpHkIBfgcF2-4w5LzZLaMOx643Xz6XCBM-iDyEVf-D6POCQOP5fjBn_h3q6cvuCgGLcsmOf7xMYtfTweNeTyVR1d-xP55Y9KAbNzTLLTQIw"
+  },
+  {
+    name: "Smart Watch",
+    desc: "Fitness tracking smartwatch with heart rate monitor.",
+    price: 4999,
+    category: "Electronics",
+    image: "https://encrypted-tbn2.gstatic.com/shopping?q=tbn:ANd9GcQ78D97_sWbYPSUc7Bo9LFmKohcmt4uUIRegUINwWTSaPgH9J5l-z2XxTm0ANNIC-QX8zo1ff830e3nXFxz-x7uKcrPdanHxHrcb97CUbED3ppcJdSNwo28qDk"
+  },
+  {
+    name: "Running Shoes",
+    desc: "Lightweight and comfortable running shoes.",
+    price: 3499,
+    category: "Footwear",
+    image: "https://encrypted-tbn2.gstatic.com/shopping?q=tbn:ANd9GcQlGaORWupb728PHsNE3PH4ta4pxonog4VCDn1zgNLakRblpvSwMTRJKALI7PmBcUHVwwI4dX8EkTl1Q-Bilw9yJdfEQ-PNLdvS92hM9M8nRD_-YUHRs_WeTQ"
+  },
+  {
+    name: "Backpack",
+    desc: "Spacious backpack for daily use and travel.",
+    price: 1299,
+    category: "Accessories",
+    image: "https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcQr0YQ7D_QqAYR0FU4m_j6YiOPeFCGFd2XObaT4MSQgAKBopnI5vvrIgagvdyw878SiwscNGfgqXD-z-QUaW4MWGygCeNqYTdECJyLY7BLSlomCZeuqFfpoYjc"
+  },
+  {
+    name: "Bluetooth Speaker",
+    desc: "Portable speaker with powerful sound quality.",
+    price: 1999,
+    category: "Electronics",
+    image: "https://encrypted-tbn3.gstatic.com/shopping?q=tbn:ANd9GcStXTbqJO0Hwvmil_0wrkmnntRkdJqKfWIq_aa98PrK_N8sNe7lg9G2sZsh4P52XVvZhCZoYH-hWnf7LC-D2_7o34gPRBUkbjOe8vB4h5sfPZewHHRIvyz5vnA"
+  },
+  {
+    name: "Water Bottle",
+    desc: "Leak-proof stainless steel water bottle.",
+    price: 299,
+    category: "Home",
+    image: "https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcQsNsLSqNb_aEQ3XDU08_6LxK0B_pFVPX9RL0kRzbSL8Wn88Yt3Xo5GFtaJz3xdJ860HVNq55KOI56C8PMIpbNl8LNxChXbBIfAdaYtJDHcVXb5jWfr2y_LpBc"
+  },
+  {
+    name: "T-Shirt",
+    desc: "Comfortable cotton t-shirt for everyday wear.",
+    price: 599,
+    category: "Fashion",
+    image: "https://encrypted-tbn3.gstatic.com/shopping?q=tbn:ANd9GcShDYVof1suss9Kw5EB23W2OJyOxL62FEmqMbtCj6jW311E6_vO7u4MNc49OLqNrmoWHuO9BY6zRbhUa0KnelWqyC_TLaLN7NIJtTOr-XWSyTgrYnjkF9UD4Q"
+  }
+  
+];

2b9fb9dc957bfced625e04633f3c2b8ac06b72f5
JAYANTI-THAKOR-941 <jyantithakor941@gmail.com>
2026-06-05 14:30:01 +0530

    calculator in js

---
+++ 03_basic.js
@@ -0,0 +1,37 @@
+// variable - container for storing data
+// ways of declare variable
+
+// automatically
+
+// example:
+    studentName = "Prem Patel";
+    console.log("studentName : ",studentName);
+
+// var
+    var age = 22;
+// let
+    // ES-6
+//    function getData(){
+//      let studentAge = 24;
+//     //  let studentAge = 25;
+//     studentAge = 45
+//    }
+//    getData();
+// const
+
+    const maxNumber = 444;
+    // maxNumber = 555;
+    console.log(maxNumber);
+
+    // primitive data types
+
+    // string  = text
+    // number = 555,22.1
+    // boolean = true/false;
+    // null = null
+    // undefined = undefind
+    // symbol = unique
+    // bigint = big numbers
+
+    // non primitive data types
+    // object
